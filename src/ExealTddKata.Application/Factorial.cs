02c1abdf12f938adffe8882ebf547002cee6c029
Stefano Vidaurre Olite <svidaurre@domingoalonsogroup.com>
2023-03-29 11:54:33 +0200

    test: the factorial should return one when input is zero

---
+++ src/ExealTddKata.Application/Factorial.cs
@@ -0,0 +1,9 @@
+﻿namespace ExealTddKata.Application;
+
+public static class Factorial
+{
+    public static int Calc(int i)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ test/ExealTddKata.Test/FactorialShould.cs
@@ -0,0 +1,14 @@
+﻿using ExealTddKata.Application;
+using FluentAssertions;
+
+namespace ExealTddKata.Test;
+
+public class FactorialShould
+{
+    [Test]
+    public void ReturnOneWhenInputIsZero()
+    {
+        int result = Factorial.Calc(0);
+        result.Should().Be(1);
+    }
+}
