76fbcdc1aa8377bd0aca29e665159e8b5b682e2e
Stefano Vidaurre Olite <svidaurre@domingoalonsogroup.com>
2023-03-30 10:06:55 +0200

    test: the fibonacci should return zero when input is zero

---
+++ src/ExealTddKata.Application/Fibonacci.cs
@@ -0,0 +1,9 @@
+﻿namespace ExealTddKata.Application;
+
+public static class Fibonacci
+{
+    public static int Calc()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ test/ExealTddKata.Test/FibonacciShould.cs
@@ -0,0 +1,18 @@
+﻿using ExealTddKata.Application;
+using FluentAssertions;
+
+namespace ExealTddKata.Test;
+
+public class FibonacciShould
+{
+    // TODO: Caso 0
+    [Test]
+    public void ReturnZeroWhenInputIsZero()
+    {
+        int result = Fibonacci.Calc();
+        result.Should().Be(0);
+    }
+    
+    // TODO: Caso 1
+    // TODO: Casos n > 1
+}
