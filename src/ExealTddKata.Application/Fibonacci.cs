822e53856d6f6cbf93ba2b72a51b8579c75320b7
Stefano Vidaurre Olite <svidaurre@domingoalonsogroup.com>
2023-03-30 10:12:30 +0200

    feat: when input is one the fibonacci class return one

--- src/ExealTddKata.Application/Fibonacci.cs
+++ src/ExealTddKata.Application/Fibonacci.cs
@@ -2,8 +2,13 @@
 
 public static class Fibonacci
 {
-    public static int Calc()
+    public static int Calc(int input)
     {
+        if (input == 1)
+        {
+            return 1;
+        }
+        
         return 0;
     }
 }
--- test/ExealTddKata.Test/FibonacciShould.cs
+++ test/ExealTddKata.Test/FibonacciShould.cs
@@ -8,14 +8,14 @@ public class FibonacciShould
     [Test]
     public void ReturnZeroWhenInputIsZero()
     {
-        int result = Fibonacci.Calc();
+        int result = Fibonacci.Calc(0);
         result.Should().Be(0);
     }
     
     [Test]
     public void ReturnOneWhenInputIsOne()
     {
-        int result = Fibonacci.Calc();
+        int result = Fibonacci.Calc(1);
         result.Should().Be(1);
     }
     // TODO: Casos n > 1
