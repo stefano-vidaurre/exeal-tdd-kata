07ab3e01d4152a94b09411072f40b67c62fcd421
Stefano Vidaurre Olite <svidaurre@domingoalonsogroup.com>
2023-03-30 10:44:18 +0200

    test: the password validator should report a numbers error

--- test/ExealTddKata.Test/PasswordValidatorShould.cs
+++ test/ExealTddKata.Test/PasswordValidatorShould.cs
@@ -13,6 +13,12 @@ public class PasswordValidatorShould
     }
     
     // TODO: Al menos 2 numeros
+    [Test]
+    public void ReportNumbersError()
+    {
+        (bool, string) result = PasswordValidator.Validate("asdfghjk");
+        result.Should().Be((false, "Password must contain at least 2 numbers"));
+    }
     // TODO: Detectar multiples errores
     // TODO: Al menos 1 mayuscula
     // TODO: Al menos 1 caracter especial
