8cab029b0192c311476b6fbf99da10f306e23643
Stefano Vidaurre Olite <svidaurre@domingoalonsogroup.com>
2023-03-30 10:48:29 +0200

    test: the password validator should return char and number error

--- test/ExealTddKata.Test/PasswordValidatorShould.cs
+++ test/ExealTddKata.Test/PasswordValidatorShould.cs
@@ -18,7 +18,13 @@ public class PasswordValidatorShould
         (bool, string) result = PasswordValidator.Validate("asdfghjk");
         result.Should().Be((false, "Password must contain at least 2 numbers"));
     }
-    // TODO: Detectar multiples errores
+
+    [Test]
+    public void ReportCharAndNumberError()
+    {
+        (bool, string) result = PasswordValidator.Validate("adad");
+        result.Should().Be((false, "Password must be at least 8 characters\nPassword must contain at least 2 numbers"));
+    }
     // TODO: Al menos 1 mayuscula
     // TODO: Al menos 1 caracter especial
 }
