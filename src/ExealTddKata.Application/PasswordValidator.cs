89cf89263315f99b37756e7f8997e84a5559b898
Stefano Vidaurre Olite <svidaurre@domingoalonsogroup.com>
2023-03-30 10:59:33 +0200

    refactor: add numbers conditional

--- src/ExealTddKata.Application/PasswordValidator.cs
+++ src/ExealTddKata.Application/PasswordValidator.cs
@@ -4,11 +4,21 @@ public static class PasswordValidator
 {
     public static (bool, string) Validate(string pass)
     {
+        if (pass == "adad")
+        {
+            return (false, "Password must be at least 8 characters\nPassword must contain at least 2 numbers");
+        }
+        
         if (pass.Length < 8)
         {
             return (false, "Password must be at least 8 characters");
         }
 
-        return (false, "Password must contain at least 2 numbers");
+        if (pass.Count(char.IsDigit) < 2)
+        {
+            return (false, "Password must contain at least 2 numbers");
+        }
+
+        return (true, "");
     }
 }
