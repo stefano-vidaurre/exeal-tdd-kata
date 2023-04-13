9b163e1d151e8a86f24e9f0c006793bc9e9c6355
Stefano Vidaurre Olite <svidaurre@domingoalonsogroup.com>
2023-04-13 11:54:05 +0200

    feat: The find method return Valencia when input is Va

--- src/ExealTddKata.Application/Cities.cs
+++ src/ExealTddKata.Application/Cities.cs
@@ -4,9 +4,14 @@ public class Cities
 {
     public IEnumerable<string> Find(string input)
     {
-        if (input == "Par")
+        if (input == "Pa")
         {
             return new[] { "Paris" };
+        }
+        
+        if (input == "Va")
+        {
+            return new[] { "Valencia" };
         }
         
         return Enumerable.Empty<string>();
