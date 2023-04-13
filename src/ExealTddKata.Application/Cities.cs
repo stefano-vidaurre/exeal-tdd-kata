ca7484d88c26ab34e4b2a4f79be66e142093df93
Stefano Vidaurre Olite <svidaurre@domingoalonsogroup.com>
2023-04-13 11:50:00 +0200

    feat: The find method return Paris when input is Par

--- src/ExealTddKata.Application/Cities.cs
+++ src/ExealTddKata.Application/Cities.cs
@@ -4,6 +4,11 @@ public class Cities
 {
     public IEnumerable<string> Find(string input)
     {
+        if (input == "Par")
+        {
+            return new[] { "Paris" };
+        }
+        
         return Enumerable.Empty<string>();
     }
 }
