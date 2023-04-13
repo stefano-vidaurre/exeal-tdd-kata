4d7889901eb15820ad9e9d76a81cdbffa15dc2c4
Stefano Vidaurre Olite <svidaurre@domingoalonsogroup.com>
2023-04-13 11:46:06 +0200

    test: the class cities should not find any cities when the input length is less than tree

--- test/ExealTddKata.Test/CitiesShould.cs
+++ test/ExealTddKata.Test/CitiesShould.cs
@@ -12,4 +12,12 @@ public class CitiesShould
         IEnumerable<string> result = cities.Find("");
         result.Should().BeEmpty();
     }
+    
+    [Test]
+    public void NotFindAnyCitiesWhenTheInputLengthIsLessThanTree()
+    {
+        Cities cities = new Cities();
+        IEnumerable<string> result = cities.Find("Pa");
+        result.Should().BeEmpty();
+    }
 }
